d4f2b4c94e56b9da1fb5bd4a59517d61fcfaec4b
hemant9898 <56092046+hemant9898@users.noreply.github.com>
2020-10-19 18:33:17 +0530

    Add files via upload

---
+++ UpdatedFliper7/Job/urls.py
@@ -0,0 +1,33 @@
+"""Fliper7 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+app_name ="Job"
+urlpatterns = [
+    path('',views.LR,name="LR" ),
+    path('home/',views.home,name="home"),
+    path('<int:user_id>filter/',views.filter, name="filter"),
+    path('test/',views.test,name="test"),
+    path('login/',views.login_view,name="login"),
+    path('register/',views.reg_view,name="register"),
+    path('logout/',views.logout_view,name="logout"),
+    path('<int:user_id>/<int:data_id>/jsp', views.jsp,name="jsp"),
+    path('<int:user_id>/user_job', views.user_job, name="user_job"),
+
+]
---
+++ UpdatedFliper7/Job/views.py
@@ -0,0 +1,187 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
+from .models import *
+from django.core.serializers import serialize
+# Create your views here.
+joke={'1','2','3','4','5','6','7','8','9','10'}
+
+def LR(request):
+	return render(request,"Job/LR.html")
+
+
+
+def home(request):
+	jj=data.objects.all()
+	paginator = Paginator(jj,6)
+
+	try:
+		page = int(request.GET.get('page',1))
+	except:
+		page=1
+
+	try:
+		posts = paginator.page(page)
+	except(EmptyPage):
+		posts = paginator.page(paginator,num_pages)
+
+	return render(request,'Job/home.html',{
+		'posts':posts,
+		})
+	
+
+
+
+def filter(request, user_id):
+	user = User.objects.get(pk=user_id)
+	if request.method == "POST":
+		datas = data.objects.all()
+
+		if request.POST["exp"]:
+			experience = int(request.POST["exp"])
+			datas = datas.filter(exp__range = (0,experience))
+
+
+		if request.POST["loc"]:
+			location = request.POST["loc"]
+			datas = datas.filter(loc = location)
+
+
+		if request.POST["skill"]:
+			skills = request.POST["skill"]
+			datas = datas.filter(skill = skills)
+
+
+		if request.POST["indus"]:
+			industory = request.POST["indus"]
+			datas = datas.filter(indus = industory)
+
+
+		if request.POST["salary"]:
+			Salary = int(request.POST["salary"])
+			datas = datas.filter(salary__range = (0,Salary))
+		
+		return render(request,'Job/test.html',{
+		'posts':datas,
+		'user':user
+		})
+	else:
+		return render(request,'Job/filter.html',{
+			'user':user
+			})
+	
+
+
+
+
+
+
+def test(request):
+	jj=data.objects.all()
+	paginator = Paginator(jj,6)
+
+	try:
+		page = int(request.GET.get('page',1))
+	except:
+		page=1
+
+	try:
+		posts = paginator.page(page)
+	except(EmptyPage):
+		posts = paginator.page(paginator,num_pages)
+
+	return render(request,'Job/test.html',{
+		'posts':posts,
+		})
+
+
+
+def login_view(request):
+	if request.method == "POST":
+		username=request.POST["username"]
+		password=request.POST["password"]
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("Job:home"))
+		else:
+			return render(request, "Job/LR.html",{
+				"msg": "Invalid"
+				})
+	return render(request,"Job/LR.html")
+
+
+
+def reg_view(request):
+	if request.method == "POST":
+		username=request.POST["username"]
+		password=request.POST["password"]
+		email = request.POST["email"]
+		rpassword = request.POST["rpassword"]
+		if password == rpassword:
+			user = User.objects.create_user(username,email,password)
+			user.save()
+			return render(request, "Job/LR.html")
+		else:
+			return render(request, "Job/LR.html",{
+				"msg":"INVALID"
+				})
+	return render(request, "Job/LR.html")
+
+
+
+def logout_view(request):
+	logout(request)
+	return render(request, "Job/LR.html")
+
+
+
+	
+	
+def jsp(request, user_id, data_id):
+	datas = data.objects.get(pk=data_id)
+	user = User.objects.get(pk=user_id)
+	datas.user.add(user)
+
+	jj=data.objects.all()
+	paginator = Paginator(jj,6)
+
+	try:
+		page = int(request.GET.get('page',1))
+	except:
+		page=1
+
+	try:
+		posts = paginator.page(page)
+	except(EmptyPage):
+		posts = paginator.page(paginator,num_pages)
+
+	return render(request,'Job/home.html',{
+		'posts':posts,
+		'user':user
+		})
+
+
+def user_job(request, user_id):
+	jj=data.objects.filter(user=user_id)
+	user = User.objects.get(pk=user_id)
+
+	paginator = Paginator(jj,6)
+
+	try:
+		page = int(request.GET.get('page',1))
+	except:
+		page=1
+
+	try:
+		posts = paginator.page(page)
+	except(EmptyPage):
+		posts = paginator.page(paginator,num_pages)
+
+	return render(request,'Job/seeking.html',{
+		'posts':posts,
+		'user':user
+		})
